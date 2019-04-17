6532d182a01d708e8c9ab3a168278a9e0ce17a6d
zunairshaikh <49729446+zunairshaikh@users.noreply.github.com>
2019-04-18 00:12:39 +0500

    adding files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function z(){
+	document.getElementById('num1').innerHTML="86%";
+}
+function u(){
+	document.getElementById('num1').innerHTML="72%";
+}
+function s(){
+	document.getElementById('num1').innerHTML="70%";
+
+}
+
